917c00752689297728f90e38c78a8bf3fb8bb2bf
Uzay <uzay00@gmail.com>
2021-11-15 10:21:52 +0300

    INF113 compute dot product

--- INF236 Programlama Uygulamaları/webapp.py
+++ INF236 Programlama Uygulamaları/webapp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('INF 236')
+st.title('--INF 236--')
 st.text('Uzay says: Welcome to INF 236')
 
 
@@ -8,5 +8,12 @@ pw = st.text_input('Enter your password')
 if st.button('Click'):
 	st.text('You have entered: ' + pw)
 
+slv = 0
+slv = st.slider('What is your Python level', 0,40, 15)
+st.write('slider value ',  slv)
 
-st.slider('What is your Python level', 0,40, step = 5)
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
+
+
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYEfTygIS8UCA-cy32F7hThYUUdc6KvE1gxw&usqp=CAU')
